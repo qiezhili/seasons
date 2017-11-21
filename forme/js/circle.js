cce617493bc42e1987d468b68c21761d99d117fe
qiezhili <2515917404@qq.com>
2017-11-21 10:20:17 +0800

    seasons
    四季网

---
+++ forme/js/circle.js
@@ -0,0 +1,126 @@
+// 圆形进度条
+$(function(){
+
+			$('#java').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj1 = $('#java').data('radialIndicator');
+			radialObj1.animate(80);
+
+			$('#spring').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj2 = $('#spring').data('radialIndicator');
+			radialObj2.animate(60);
+
+			$('#html').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj3 = $('#html').data('radialIndicator');
+			radialObj3.animate(85);
+			
+			
+			
+			$('#javascript').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj4 = $('#javascript').data('radialIndicator');
+			radialObj4.animate(75);
+			
+			
+			$('#jquery').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj5 = $('#jquery').data('radialIndicator');
+			radialObj5.animate(70);
+			
+			
+			$('#bootstrap').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj6 = $('#bootstrap').data('radialIndicator');
+			radialObj6.animate(75);
+			
+			
+			$('#less').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj7 = $('#less').data('radialIndicator');
+			radialObj7.animate(65);
+			
+			
+			$('#ps').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj8 = $('#ps').data('radialIndicator');
+			radialObj8.animate(30);
+			
+			
+			$('#axture').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj9 = $('#axture').data('radialIndicator');
+			radialObj9.animate(50);
+			
+			
+			$('#css').radialIndicator({
+				radius:70,
+				barColor: '#ff675f',
+				barWidth: 10,
+				initValue: 0,
+				roundCorner : true,
+				percentage: true
+			});
+			var radialObj10 = $('#css').data('radialIndicator');
+			radialObj10.animate(70);
+			
+			
+			
+
+	
+})
